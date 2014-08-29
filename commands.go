6baab98499ef98d4376d09776e94b5bbdf3110aa
Takashi Masuda <masutaka.net@gmail.com>
2014-08-29 15:26:26 +0900

    $ cli-init -s add,list,delete sample-cli-init

---
+++ commands.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+var Commands = []cli.Command{
+	commandAdd,
+	commandList,
+	commandDelete,
+}
+
+var commandAdd = cli.Command{
+	Name:  "add",
+	Usage: "",
+	Description: `
+`,
+	Action: doAdd,
+}
+
+var commandList = cli.Command{
+	Name:  "list",
+	Usage: "",
+	Description: `
+`,
+	Action: doList,
+}
+
+var commandDelete = cli.Command{
+	Name:  "delete",
+	Usage: "",
+	Description: `
+`,
+	Action: doDelete,
+}
+
+func debug(v ...interface{}) {
+	if os.Getenv("DEBUG") != "" {
+		log.Println(v...)
+	}
+}
+
+func assert(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func doAdd(c *cli.Context) {
+}
+
+func doList(c *cli.Context) {
+}
+
+func doDelete(c *cli.Context) {
+}
